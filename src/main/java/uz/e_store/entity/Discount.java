21bad10c87b2f177e6ebf83d99c07f653fde052a
Jonibek <ashirov.uzbek.1997@gmail.com>
2022-03-30 12:35:56 +0500

    dscsdcds

--- src/main/java/uz/e_store/entity/Discount.java
+++ src/main/java/uz/e_store/entity/Discount.java
@@ -3,8 +3,11 @@ package uz.e_store.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.convert.threetenbp.ThreeTenBackPortJpaConverters;
 import uz.e_store.entity.template.AbsEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 
@@ -13,8 +16,10 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @Entity
 public class Discount extends AbsEntity {
+
     private float percent;
 
+    @Column(name = "expiration_date",nullable = false)
     private Timestamp expirationDate;
 
     private boolean delete;
